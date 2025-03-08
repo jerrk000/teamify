d51e01f33659c5d7c8284d0a7649179c4a3b0717
Jeremias Kühberger <jkuehberger@protonmail.com>
2025-03-08 19:32:22 +0100

    fixed type-unclarities and changed names for project

--- frontend/Teamify/store/useListStore.ts
+++ frontend/Teamify/store/useListStore.ts
@@ -1,10 +1,16 @@
 import { create } from "zustand";
 
+type Item = {
+  id: string;
+  name: string;
+};
+ 
 interface ListStore {
-  items: string[];
-  setItems: (items: string[]) => void;
+  items: Item[];
+  setItems: (items: Item[]) => void;
+ 
 }
-
+ 
 export const useListStore = create<ListStore>((set) => ({
   items: [],
   setItems: (items) => set({ items }),
